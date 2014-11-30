32c40b39f2933b69dcc936552fe27e74602e22dc
IrleiNascimento <irlei.inacio@gmail.com>
2014-11-29 23:10:35 -0200

    Update Lista_Duplamente_Encadeada.c

--- Estrutura_de_dados/LISTAS_TRABALHO_FINAL/Lista_Duplamente_Encadeada.c
+++ Estrutura_de_dados/LISTAS_TRABALHO_FINAL/Lista_Duplamente_Encadeada.c
@@ -1,115 +1,126 @@
-/*
- * 
+/* 
+ * Lista_Duplamente_Encadeada.c
+ * Author: Irlei
  *
- *  
- *      Author: Irlei
+ * Created on 20 de Março de 2013, 20:20
  */
-#include<stdio.h>
-#define MAXTAM 20
+
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef int TipoChave;
 
 typedef struct {
 	TipoChave chave;
 } TipoItem;
 
+typedef struct TipoCelula *Apontador;
+
+typedef struct TipoCelula {
+	TipoItem Item;
+	Apontador Prox, Ant;
+} celula;
+
 typedef struct {
-	TipoItem itens[MAXTAM];
-	int left;
-	int right;
-} Deque;
+	Apontador Primeiro, Ultimo;
+	int tamanho;
+} TipoLista;
 
-void inicia_deque(Deque *deque);
+void Inicia(TipoLista *Lista);
+void Insere(TipoItem x, TipoLista *Lista);
+void Retira(Apontador p, TipoLista *Lista, TipoItem *item);
+int Vazia(TipoLista *Lista);
+void Imprime(TipoLista Lista);
 
-int deque_Vazio(Deque deque);
-
-void insertRight(TipoItem x, Deque *deque);
-void insertLeft(TipoItem x, Deque *deque);
-void removLeft(Deque *deque, TipoItem *item);
-void removRight(Deque *deque, TipoItem *item);
-
-void inicia_deque(Deque *deque) {
-	deque->left = -1;
-	deque->right = MAXTAM;
+void Inicia(TipoLista *Lista) {
+	Lista->Primeiro = (Apontador) malloc(sizeof(celula));
+	Lista->Ultimo = Lista->Primeiro;
+	Lista->Ultimo->Ant = Lista->Primeiro;
+	Lista->Primeiro->Prox = Lista->Ultimo;
+	Lista->tamanho = 0;
 }
 
-int deque_Vazio(Deque deque) {
-	return (deque.left == -1 && deque.right == MAXTAM);
-}
-
-void insertRight(TipoItem x, Deque *deque) {
-	int t = deque->left;
-	if (deque->right - 1 == deque->left) {
-		printf("Deque cheio\n");
-		return;
-	} else {
-		deque->right--;
-		deque->itens[deque->right] = x;
-	}
-	deque->left = t;
-}
-
-void insertLeft(TipoItem x, Deque *deque) {
-	if (deque->left + 1 == deque->right) {
-		printf("Deque cheio\n");
-		return;
-	} else {
-		deque->left++;
-		deque->itens[deque->left] = x;
-	}
-}
-void removLeft(Deque *deque, TipoItem *item) {
-
-	if (deque_Vazio(*deque)) {
-		printf("Erro   pilha esta  Vazia\n");
-	} else {
-		*item = deque->itens[deque->left];
-		deque->left--;
-	}
-}
-void removRight(Deque *deque, TipoItem *item) {
-	if (deque_Vazio(*deque)) {
-		printf("Deque vazio\n");
-	} else {
-		*item = deque->itens[deque->right];
-		deque->right++;
-	}
+void Insere(TipoItem x, TipoLista *Lista) {
+	Apontador aux = Lista->Ultimo;
+	Lista->Ultimo->Prox = (Apontador) malloc(sizeof(celula));
+	Lista->Ultimo = Lista->Ultimo->Prox;
+	Lista->Ultimo->Item = x;
+	Lista->Ultimo->Ant = aux;
+	Lista->Ultimo->Prox = Lista->Primeiro;
+	Lista->Primeiro->Ant = Lista->Ultimo;
+	Lista->tamanho = Lista->tamanho + 1;
 
 }
-
-void imprime(Deque deque) {
-	if (deque_Vazio(deque)) {
-		printf("Deque vazio\n");
+void Retira(Apontador p, TipoLista *Lista, TipoItem *item) {
+	Apontador pAux;
+	if (Vazia(Lista) || p == Lista->Primeiro || p->Prox == Lista->Primeiro) {
+		printf(" Erro Lista Vazia ou posicao nao existe\n");
 		return;
 	}
-	int aux = 0;
-	aux = deque.left;
-	while (aux > -1) {
-		printf("Chave : %d  left    %d\n", deque.itens[aux].chave, aux);
-		aux--;
-	}
-	aux = deque.right;
-	while (aux < MAXTAM) {
-		printf("Chave : %d right    %d\n", deque.itens[aux].chave, aux);
-		aux++;
+	pAux = p->Prox;
+	*item = pAux->Item;
+	p->Prox = pAux->Prox;
+	p->Prox->Ant = p;
+	if (p->Prox == Lista->Primeiro)
+		Lista->Ultimo = p;
+	free(pAux);
+	Lista->tamanho = Lista->tamanho - 1;
+}
+
+int Vazia(TipoLista *Lista) {
+	return (Lista->Primeiro == Lista->Ultimo);
+}
+
+void Imprime(TipoLista Lista) {
+	Apontador Aux;
+	Aux = Lista.Primeiro->Prox;
+	printf("  ANT                 CHAVE                PROX  \n\n");
+	while (Aux != Lista.Primeiro) {
+		printf("  %d            %d             %d\n", Aux->Ant->Item.chave, Aux->Item.chave,
+				Aux->Prox->Item.chave);
+		Aux = Aux->Prox;
 	}
 }
 
-int main() {
-	TipoItem its[15] = { 2, 5, 63, 7, 3 ,5,4,8,9,55,50,75,6,79,31};
-	TipoItem it1s[15] = { 10, 56, 80, 23, 54 ,51,67,73,38,34,57,78,90,12,24};
-	Deque dq;
-	inicia_deque(&dq);
+int main(int argc, char **argv) {
+	printf(" \nTESTE  COM 10 ITENS, CHAVE DE 1 A 10 \n\n");
+	TipoItem it[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	TipoLista l1;
+	Inicia(&l1);
+
 	int i;
-	for (i = 0; i < 15; i++) {
-
-		insertLeft(its[i], &dq);
-		insertRight(it1s[i], &dq);
+	for (i = 0; i < 10; i++) {
+		Insere(it[i], &l1);
+	}
+	Imprime(l1);
+	printf("\nAPONTAR PARA REMOCAO ITENS DE CHAVE : 3 ,4, 8,9\n");
+	printf("\n");
+	Apontador p = l1.Primeiro->Prox ;
+	TipoItem it0;
+	while(p != l1.Primeiro){
+		if(p->Prox->Item.chave == 3){
+			Retira(p, &l1, &it0);
+	printf(" Chave %d  removida\n", it0.chave);
+		}
+		if(p->Prox->Item.chave == 4){
+				Retira(p, &l1, &it0);
+		printf(" Chave %d  removida\n", it0.chave);
+		}
+		if(p->Prox->Item.chave == 8){
+				Retira(p, &l1, &it0);
+		printf(" Chave %d  removida\n", it0.chave);
+		}
+		if(p->Prox->Item.chave == 9){
+				Retira(p, &l1, &it0);
+		printf(" Chave %d  removida\n", it0.chave);
+		}
+		p=p->Prox;
 	}
 
-	TipoItem it;
-	imprime(dq);
-	printf("\n\n");
-	removLeft(&dq,&it);
-	removRight(&dq,&it);
-	imprime(dq);
+	printf("\nLISTA POS REMOCAO\n\n");
+	Imprime(l1);
+
+	printf("\nObs: O numero grande no inicio/fim da lista e o ponteiro do no cabeca\napenas para ficar claro que se trata de uma fila circular\n\n\n");
+	return 0;
 }
+
